3f733b444f1c15ce571a6174291234d726c40001
ckuzmick <conrad.kuzmick@icloud.com>
2021-08-31 15:27:10 -0400

    index update

--- src/pages/index.js
+++ src/pages/index.js
@@ -99,10 +99,36 @@ function FadeVis({ children }) {
       ref={ref}
       animate={controls}
       initial="hidden"
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 1 }}
       variants={{
         visible: { opacity: 1, scale: 1 },
-        hidden: { opacity: 0, scale: 0 }
+        hidden: { opacity: 0, scale: 1 }
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+function SlideIn({ children }) {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      transition={{ duration: 1 }}
+      variants={{
+        visible: { position: 200 },
+        hidden: { position: -200  }
       }}
     >
       {children}
@@ -142,6 +168,21 @@ Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. Maecenas 
 
 </FadeVis>
 
+<SlideIn>
+
+<ul>
+  <li>list item</li>
+  <li>list item</li>
+  <li>list item</li>
+  <li>list item</li>
+  <li>list item</li>
+  <li>list item</li>
+  <li>list item</li>
+  <li>list item</li>
+  <li>list item</li>
+</ul>
+
+</SlideIn>
 
     </main>
   )
